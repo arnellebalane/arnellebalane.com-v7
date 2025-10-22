7c4e2d12c4e565ee3eb5cbd97fac412d74cd16e2
Arnelle Balane <arnellebalane@gmail.com>
2025-10-21 23:18:07 -0700

    Define astro collection for experiences data

---
+++ src/content/config.ts
@@ -0,0 +1,17 @@
+import { defineCollection, z } from 'astro:content';
+import { file } from 'astro/loaders';
+
+const experiences = defineCollection({
+    loader: file('data/experiences.json'),
+    schema: z.object({
+        position: z.string(),
+        company: z.string(),
+        start_date: z.string(),
+        end_date: z.string().optional(),
+        is_present: z.boolean(),
+        image_url: z.string(),
+        details: z.array(z.string()),
+    }),
+});
+
+export const collections = { experiences };
